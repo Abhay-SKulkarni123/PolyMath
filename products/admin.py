c730e1c1e6383d6d2bd63189b18fdc885c3f3d66
Abhay S Kulkarni <Abhay-SKulkarni123@users.noreply.github.com>
2026-05-12 11:22:20 +0530

    feat: add Category and Product models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display= ['name', 'created_at']
+    search_fields = ['name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display= ['name', 'vendor', 'price', 'stock', 'is_active']
+    list_filter = ['is_active', 'categories']
+    search_fields = ['name', 'vendor__store_name']
+
--- products/models.py
+++ products/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.TextField(max_length=255, unique=True)
+    description = models.CharField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category' 
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    vendor = models.ForeignKey(
+        'vendors.VendorProfile',
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    categories = models.ManyToManyField(
+        Category,
+        related_name = 'products',
+        blank=True
+    )
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Products'
+        verbose_name_plural = 'Products'
+
+        def __str__(self):
+            return self.name
