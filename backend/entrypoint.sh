a65ce5ddaae89c607a78a4d19cf235d5f7b06908
Abhay S Kulkarni <Abhay-SKulkarni123@users.noreply.github.com>
2026-06-29 12:01:22 +0530

    Fixed Migrations

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,17 +4,20 @@ set -e
 echo "Running migrations..."
 python manage.py migrate
 
+echo "Seeding Knowledge Fields..."
+python manage.py seed_knowledge_fields || true
+
 echo "Seeding cinema data..."
 python manage.py seed_cinema || true
-
-echo "Fixing All In One..."
-python manage.py fix_all_in_one || true
 
 echo "Creating vendor & seeding books..."
 python manage.py create_vendor_seed_book || true
 
 echo "Seeding featured books..."
 python manage.py seed_featured_books || true
+
+echo "Fixing All In One..."
+python manage.py fix_all_in_one || true
 
 echo "Starting Gunicorn..."
 
